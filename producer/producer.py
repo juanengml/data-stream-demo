7c2867cdec35b729e01892a9be4924df4329b94f
Sr Marinho <37448831+juanengml@users.noreply.github.com>
2021-08-01 18:15:22 -0300

    Update producer.py

--- producer/producer.py
+++ producer/producer.py
@@ -25,8 +25,6 @@ queue = Queue(name=_queue, exchange=exchange, routing_key=_routing_key)
 queue.maybe_bind(conn)
 queue.declare()
 	
-endpoint = "https://random-data-api.com/api/address/random_address"
-
 data = {"size":100}
 
 df = pd.read_csv("https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv")
@@ -34,24 +32,20 @@ df = pd.read_csv("https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7
 def data_iris():
     
   data = {"sepal_length": choice(df['sepal_length'].tolist()),
-        "sepal_width":  choice(df['sepal_width'].tolist()), 
-        "petal_length": choice(df['petal_length'].tolist()),
-        "petal_width":  choice(df['petal_width'].tolist())
+          "sepal_width":  choice(df['sepal_width'].tolist()), 
+          "petal_length": choice(df['petal_length'].tolist()),
+          "petal_width":  choice(df['petal_width'].tolist())
        }
   return data
 
 
 def main():
     while True:
-           #try:
-              #r = get(endpoint,data,verify=True).json() 
-              #for p in r:
-              data = data_iris()
+	  for p in range(10000000): 
+	      data = data_iris()
               producer.publish(data,serializer='pickle', compression='bzip2', routing_key=_routing_key)
               print(data)
-           #except:
-           #   pass 
-
+	  sleep(5*60)	# delay de 5 mins 
 
 if __name__ == "__main__":
     main()
