e1ed1f4ef72ad804bafb2929e9cfedb42bc3fa89
Sr Marinho <37448831+juanengml@users.noreply.github.com>
2021-08-01 18:17:33 -0300

    Update producer.py

--- producer/producer.py
+++ producer/producer.py
@@ -9,24 +9,18 @@ console = Console()
 
 console.log("PRODUCER APP....")
 
-rabbit_url =  'amqp://guest:guest@192.168.0.17:5672//'
-
-conn = Connection(rabbit_url)
+conn = Connection('amqp://guest:guest@192.168.0.17:5672//')
 channel = conn.channel()
 
-_exchange = "data-adress-exchange"
-_queue = "data-adress-queue"
 _routing_key = "routing-key-adress"
 
-exchange = Exchange(_exchange, type="direct", delivery_mode=1)
+exchange = Exchange("data-adress-exchange", type="direct", delivery_mode=1)
 producer = Producer(exchange=exchange, channel=channel, routing_key=_routing_key)
-queue = Queue(name=_queue, exchange=exchange, routing_key=_routing_key)
+queue = Queue(name="data-adress-queue", exchange=exchange, routing_key=_routing_key)
 
 queue.maybe_bind(conn)
 queue.declare()
 	
-data = {"size":100}
-
 df = pd.read_csv("https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv")
 
 def data_iris():
@@ -44,7 +38,8 @@ def main():
 	  for p in range(10000000): 
 	      data = data_iris()
               producer.publish(data,serializer='pickle', compression='bzip2', routing_key=_routing_key)
-              print(data)
+              console.info(data)
+	  console.log("DELAY...")
 	  sleep(5*60)	# delay de 5 mins 
 
 if __name__ == "__main__":
