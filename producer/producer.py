b1557282d33739457e7841c73fdca176c8bf96a1
Sr Marinho <37448831+juanengml@users.noreply.github.com>
2021-08-01 21:35:24 +0000

    testar producer

--- producer/producer.py
+++ producer/producer.py
@@ -1,5 +1,4 @@
-import streamlit as st 
-from requests import get 
+import schedule ,time
 import pandas as pd
 from random import choice
 from kombu import Connection, Exchange, Producer, Queue
@@ -7,40 +6,54 @@ from console_logging.console import Console
 
 console = Console()
 
-console.log("PRODUCER APP....")
+## conecta com rabbit 
+conn = Connection('amqp://guest:guest@http://ec2-3-139-68-72.us-east-2.compute.amazonaws.com/:5672//')
 
-conn = Connection('amqp://guest:guest@192.168.0.17:5672//')
+# define nosso canal
 channel = conn.channel()
 
 _routing_key = "routing-key-adress"
 
+# define nossa exchange e producer e fila 
 exchange = Exchange("data-adress-exchange", type="direct", delivery_mode=1)
 producer = Producer(exchange=exchange, channel=channel, routing_key=_routing_key)
 queue = Queue(name="data-adress-queue", exchange=exchange, routing_key=_routing_key)
 
+# declara nossa fila na conexão
 queue.maybe_bind(conn)
 queue.declare()
-	
+
+# le nossa dataset 
 df = pd.read_csv("https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv")
 
+## função que gera pega os dados das colunas da base iris.csv
 def data_iris():
-    
-  data = {"sepal_length": choice(df['sepal_length'].tolist()),
+  return  {"sepal_length": choice(df['sepal_length'].tolist()),
           "sepal_width":  choice(df['sepal_width'].tolist()), 
           "petal_length": choice(df['petal_length'].tolist()),
           "petal_width":  choice(df['petal_width'].tolist())
        }
-  return data
+
+## funcao que pega os dados da funcao e publica no rabbit
+def job():
+    for p in range(10000000): 
+	            data = data_iris()
+              producer.publish(data,
+                               serializer='pickle', 
+                               compression='bzip2',
+                               routing_key=_routing_key)
+              console.info(data)
 
 
+## a cada 5 min dispara o job
+schedule.every(5).minutes.do(job)
+
+## função main 
 def main():
     while True:
-	  for p in range(10000000): 
-	      data = data_iris()
-              producer.publish(data,serializer='pickle', compression='bzip2', routing_key=_routing_key)
-              console.info(data)
-	  console.log("DELAY...")
-	  sleep(5*60)	# delay de 5 mins 
+        schedule.run_pending()
+        time.sleep(1)
+        console.info("aguardando...")
 
 if __name__ == "__main__":
     main()
